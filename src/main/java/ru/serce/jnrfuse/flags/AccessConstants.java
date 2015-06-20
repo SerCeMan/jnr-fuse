491228d3748bdf89744a00ffc6f15aad149dc4ba
Sergey Tselovalnikov <sergeicelov@gmail.com>
2015-06-20 16:16:30 +0300

    Fix versions

--- src/main/java/ru/serce/jnrfuse/flags/AccessConstants.java
+++ src/main/java/ru/serce/jnrfuse/flags/AccessConstants.java
@@ -1,18 +1,19 @@
 package ru.serce.jnrfuse.flags;
 
+import jnr.constants.platform.Access;
+
 /**
  * Values for the second argument to access. These may be OR'd together.
  * <p>
- * TODO: update JNR_CONSTANTS TO 0.8.8, and replace this to src/main/java/jnr/constants/platform/Access.java
  *
  * @author Sergey Tselovalnikov
  * @since 05.06.15
  */
 public final class AccessConstants {
-    public static final int R_OK = 4; /* Test for read permission.  */
-    public static final int W_OK = 2; /* Test for write permission.  */
-    public static final int X_OK = 1; /* Test for execute permission.  */
-    public static final int F_OK = 0; /* Test for existence.  */
+    public static final int R_OK = Access.R_OK.intValue(); /* Test for read permission.  */
+    public static final int W_OK = Access.W_OK.intValue(); /* Test for write permission.  */
+    public static final int X_OK = Access.X_OK.intValue(); /* Test for execute permission.  */
+    public static final int F_OK = Access.F_OK.intValue(); /* Test for existence.  */
 
     private AccessConstants() {}
 }
