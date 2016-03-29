d06109062b89fdf96d36bc2ae4b54190ab8d8ae9
George Kankava <george.kankava@devfactory.com>
2016-03-29 15:36:05 +0400

    squid:S1213 - The members of an interface declaration or class should appear in a pre-defined order

--- src/main/java/ru/serce/jnrfuse/struct/FusePollhandle.java
+++ src/main/java/ru/serce/jnrfuse/struct/FusePollhandle.java
@@ -17,15 +17,15 @@ import jnr.ffi.BaseStruct;
  * @since 02.06.15
  */
 public class FusePollhandle extends BaseStruct {
-    protected FusePollhandle(jnr.ffi.Runtime runtime) {
-        super(runtime);
-    }
-
     public final Unsigned64 kh = new Unsigned64();
     // TODO struct fuse_chan *ch;
     public final Pointer ch = new Pointer();
     // TODO struct fuse_ll *f;
     public final Pointer f = new Pointer();
+
+    protected FusePollhandle(jnr.ffi.Runtime runtime) {
+        super(runtime);
+    }
 
     public static FusePollhandle of(jnr.ffi.Pointer pointer) {
         FusePollhandle ph = new FusePollhandle(jnr.ffi.Runtime.getSystemRuntime());
