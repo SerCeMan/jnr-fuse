25e98e3d9f4f40fce38119e132aeda45a9a5c436
Michael Hartmeier <mlhartme@github.com>
2017-05-11 10:54:21 +0200

    added more fields

--- src/main/java/ru/serce/jnrfuse/struct/FuseContext.java
+++ src/main/java/ru/serce/jnrfuse/struct/FuseContext.java
@@ -4,16 +4,20 @@ import jnr.ffi.BaseStruct;
 import jnr.ffi.Runtime;
 
 public class FuseContext extends BaseStruct {
+    public final Pointer fuse = new Pointer();
     public final uid_t uid = new uid_t();
     public final gid_t gid = new gid_t();
     public final pid_t pid = new pid_t();
     public final Pointer private_data = new Pointer();
+    public final mode_t umask = new mode_t();
 
     public FuseContext(Runtime runtime) {
         super(runtime);
     }
 
-/*    public java.lang.String toString() {
-        return "uid=" + uid.get() + " gid=" + gid.get() + " pid=" + pid.get();
-    }*/
+    public static FuseContext of(jnr.ffi.Pointer pointer) {
+        FuseContext fc = new FuseContext(jnr.ffi.Runtime.getSystemRuntime());
+        fc.useMemory(pointer);
+        return fc;
+    }
 }
