27ca0eafb348dad2a7c7c5242d02a73b5808db92
Sergey Tselovalnikov <sergeicelov@gmail.com>
2017-12-03 17:37:33 +1100

    Remove improper error handling

--- src/main/java/ru/serce/jnrfuse/utils/MountUtils.java
+++ src/main/java/ru/serce/jnrfuse/utils/MountUtils.java
@@ -11,19 +11,19 @@ public class MountUtils {
      * Perform/force a umount at the provided Path
      */
     public static void umount(Path mountPoint) {
+        String mountPath = mountPoint.toAbsolutePath().toString();
         try {
-            String mountPath = mountPoint.toAbsolutePath().toString();
-            try {
-                new ProcessBuilder("fusermount", "-u", "-z", mountPath).start();
-            } catch (IOException e) {
-                try {
-                    new ProcessBuilder("umount", mountPath).start().waitFor();
-                } catch (InterruptedException e1) {
-                    e1.printStackTrace();
-                }
-            }
+            new ProcessBuilder("fusermount", "-u", "-z", mountPath).start();
         } catch (IOException e) {
-            throw new FuseException("Unable to umount FS", e);
+            try {
+                new ProcessBuilder("umount", mountPath).start().waitFor();
+            } catch (InterruptedException ie) {
+                Thread.currentThread().interrupt();
+                throw new FuseException("Unable to umount FS", e);
+            } catch (IOException ioe) {
+                ioe.addSuppressed(e);
+                throw new FuseException("Unable to umount FS", ioe);
+            }
         }
     }
 }
